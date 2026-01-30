e078f82e4c43f0dc58971c5ff436e3bc4191d494
Ba Nguyễn <banx9x@gmail.com>
2026-01-30 10:11:41 +0700

    Test auto update

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -30,7 +30,7 @@ const items: TabsProps["items"] = [
 const App = () => {
   return (
     <Flex vertical gap="middle">
-      <h1>Face Attendance System</h1>
+      <h1>Test Auto Update</h1>
 
       <Tabs defaultActiveKey="1" items={items} />
     </Flex>

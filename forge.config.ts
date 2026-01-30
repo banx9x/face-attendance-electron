d7d300ccd9135f4d1595d217be15019eb5706986
Ba Nguyễn <banx9x@gmail.com>
2026-01-30 11:04:51 +0700

    Fix auto update

--- forge.config.ts
+++ forge.config.ts
@@ -11,7 +11,7 @@ import { PublisherGithub } from "@electron-forge/publisher-github";
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
-    extraResource: ["dist/server"],
+    extraResource: ["dist/server", "app-update.yml"],
     icon: "./src/client/assets/icon",
   },
   rebuildConfig: {},
